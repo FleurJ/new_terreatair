41596638142ac5df077f111698aa2e144b7511ee
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-12-27 17:46:49 +0100

    add activities to schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
   before_action :find_schedule, only: [:update, :edit, :destroy]
   before_action :authorised_user, only: [:new, :create, :update, :edit, :destroy]
+  before_action :find_activities, only: [:new, :edit]
 
   def new
     @schedule = Schedule.new
@@ -33,7 +34,7 @@ class SchedulesController < ApplicationController
   private
 
   def schedule_params
-    params.require(:schedule).permit(:status, :schedule_date, :classes_qty)
+    params.require(:schedule).permit(:status, :schedule_date, :classes_qty, :activity)
   end
 
   def authorised_user
@@ -42,5 +43,9 @@ class SchedulesController < ApplicationController
 
   def find_schedule
     @schedule = Schedule.find(params[:id])
+  end
+
+  def find_activities
+    @activities = Activity.all
   end
 end
---
+++ test/models/schedules_activity_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SchedulesActivityTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
