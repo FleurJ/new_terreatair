c0ecd9280aa4092eb3da3e059fecce1102865034
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-12-27 01:22:37 +0100

    crud activities

---
+++ app/models/activity.rb
@@ -0,0 +1,13 @@
+class Activity < ApplicationRecord
+  ALLOWED_STATUSES = %w[draft published archived].freeze
+
+  belongs_to :user
+
+  has_rich_text :body
+  has_one_attached :img_header
+  has_one_attached :img_thumbnail
+
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: { in: ALLOWED_STATUSES,
+                                                  message: "%{value} n'est pas un statut valide" }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_230114) do
+ActiveRecord::Schema.define(version: 2020_12_27_001550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,22 @@ ActiveRecord::Schema.define(version: 2020_12_26_230114) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "activities", force: :cascade do |t|
+    t.string "title"
+    t.string "activity_type"
+    t.string "public"
+    t.float "price"
+    t.string "duration"
+    t.string "links"
+    t.string "language"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.text "teaser"
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
   create_table "contents", force: :cascade do |t|
     t.string "title"
     t.string "status"
@@ -73,6 +89,18 @@ ActiveRecord::Schema.define(version: 2020_12_26_230114) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "titles", force: :cascade do |t|
+    t.string "type"
+    t.string "public"
+    t.float "price"
+    t.string "duration"
+    t.string "links"
+    t.string "language"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,6 +117,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_230114) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "activities", "users"
   add_foreign_key "contents", "users"
   add_foreign_key "contents_tags", "contents"
   add_foreign_key "contents_tags", "tags"
