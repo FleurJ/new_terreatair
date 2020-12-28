b7ca4fbd099e89ebdd5cf702f734344e174abec4
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-12-28 18:01:14 +0100

    crud products

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :schedules
     resources :users
     resources :memberships
+    resources :products
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_162013) do
+ActiveRecord::Schema.define(version: 2020_12_28_165533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 2020_12_28_162013) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "product_type"
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
