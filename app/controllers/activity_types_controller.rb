938fdfa1958468d8b6a925e4dd7ab4fa18d1d0bd
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-12-29 12:52:23 +0100

    crud-activity-types

--- app/controllers/activity_types_controller.rb
+++ app/controllers/activity_types_controller.rb
@@ -1,2 +1,46 @@
 class ActivityTypesController < ApplicationController
+  before_action :find_activity_type, only: [:update, :edit, :destroy]
+  before_action :authorised_user
+
+  def index
+    @activity_types = ActivityType.all
+  end
+
+  def new
+    @activity_type = ActivityType.new
+  end
+
+  def create
+    @activity_type = ActivityType.new(activity_type_params)
+    @activity_type.user = current_user
+    @activity_type.save
+    redirect_to activity_types_path
+  end
+
+  def edit
+  end
+
+  def update
+    @activity_type.update!(activity_type_params)
+    redirect_to activity_types_path
+  end
+
+  def destroy
+    @activity_type.destroy
+    redirect_to activity_types_path
+  end
+
+  private
+
+  def find_activity_type
+    @activity_type = ActivityType.find(params[:id])
+  end
+
+  def activity_type_params
+    params.require(:activity_type).permit(:title, :status)
+  end
+
+  def authorised_user
+    return true if current_user.admin == true
+  end
 end
--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,2 +1,9 @@
 class ActivityType < ApplicationRecord
+  ALLOWED_STATUSES = %w[draft published archived].freeze
+
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: { in: ALLOWED_STATUSES,
+                                                  message: "%{value} n'est pas un statut valide" }
 end
