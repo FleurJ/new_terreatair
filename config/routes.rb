497252d9175dc407ff7ab042a4297a15ecb1a974
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2021-11-01 10:18:36 +0100

    push update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-    devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
 
   scope '(:locale)', locale: /fr|nl|en/ do
     root to: 'pages#home'
@@ -7,11 +8,12 @@ Rails.application.routes.draw do
     resources :contents
     resources :activities
     resources :schedules
-    resources :users
     resources :memberships
     resources :products
     resources :stocks
     resources :activitytypes
+    resources :registrations
+
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
